c7cb6f410c73437485bad6e66a6c770ffcd4b8e2
saher90 <sahernaoum@gmail.com>
2020-08-04 20:21:18 +0300

    ready

--- load-db.js
+++ load-db.js
@@ -3,35 +3,35 @@
 /***************************************************/
 
 const MongoClient = require('mongodb').MongoClient;
-const assert = require('assert'); 
+const assert = require('assert');
 
 const fs = require('fs');
 
 const url = process.env.MONGODB_URI || 'mongodb://localhost:27017'; // mongodb Connection URL
 const dbName = 'heroku_9200wlz6'; // Database Name
- 
+
 
 
 // Use connect method to connect to the server
 MongoClient.connect(url, function(err, client) {
-    
+
     assert.equal(null, err);
     console.log("Connected successfully to MongoDB server");
-        
+
     const db = client.db(dbName);
 
-    fs.readFile('capitals.json', (err, data) => {
-        
+    fs.readFile('products.json', (err, data) => {
+
         if (err) throw err;
-        let capitals = JSON.parse(data);    // read data from json file
-            
-        const collection = db.collection('countries');      // Get the countries collection
+        let capitals = JSON.parse(data); // read data from json file
+
+        const collection = db.collection('products'); // Get the countries collection
         console.log(capitals);
-        collection.insertMany(capitals, function(err, result) {  // Insert documents
+        collection.insertMany(capitals, function(err, result) { // Insert documents
             assert.equal(err, null);
             console.log(result);
-        });        
-    
+        });
+
     });
 
 });
